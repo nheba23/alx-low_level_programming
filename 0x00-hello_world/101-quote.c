e3e6c836362b6043e5a12f9c509e943ad6d786f6
nheba23 <nourhiba68@gmail.com>
2023-02-16 11:04:05 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry
- * Return:1
+ * Return: 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
