1d24619e2a56507a279e019d94d892e9a6e1047a
nheba23 <nourhiba68@gmail.com>
2023-02-17 13:11:56 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (sucess)
+ */
+int main (void)
+{
+	char c;
+	c = 'z';
+	while 
+		(c <= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
