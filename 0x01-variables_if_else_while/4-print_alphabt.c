2f89c03194bc6545bff469a68c3fa08e783f7a83
nheba23 <nourhiba68@gmail.com>
2023-02-17 12:35:18 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char c;	
+c = 'a';	
+	while 
+	(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
