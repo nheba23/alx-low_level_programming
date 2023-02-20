fc2d3e8cbd28f3696525b3980f8b1990bebc2111
nheba23 <nourhiba68@gmail.com>
2023-02-20 06:41:40 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
 	char c;
-	c = 'a';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++
-		}
+	for (c = 'a'; c <= 'z'; ++c)
+
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
