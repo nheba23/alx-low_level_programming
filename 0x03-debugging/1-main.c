a19bf29a4d75e1882b71b371b0bd22f7d58f8669
nheba23 <nourhiba68@gmail.com>
2023-02-23 05:27:23 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causing the output to go into an infinite loop
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+
+printf("Ifinite loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("infinite loop avoided! \\o/\n");
+return (0);
+}
