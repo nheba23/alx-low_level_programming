1a09386286f3c3e7e5e4eaa99bc94211e18ead0f
nheba23 <nourhiba68@gmail.com>
2023-02-23 10:31:15 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-int x, y;
+	int x, y;
 
 for (x = 0; x < 10; x++)
-{
-for (y = 0; y <= 14; y++)
-{
-if (y > 0)
-{
-_putchar((y / 10) + '0');
-}
-_putchar((y % 10) + '0');
-}
-_putchar('\n');
-}
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 0)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
 }
