299bb505bdf02be49972fa707c06b8bfc788ddfb
nheba23 <nourhiba68@gmail.com>
2023-03-06 13:15:51 -0800

    void set_string(char **s, char *to)

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - Sets the vaue of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
